690b903b373ee64eb2c1796080801b5184a34b34
varun1505 <varun1505@gmail.com>
2013-09-19 04:08:41 +0530

    Fetch Excuses based on category clicked

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-var url = 'http://localhost/excuse/api/';
+//var url = 'http://vps.wpcoach.in/dev/excusepro-web/api/';
+var url = 'http://192.168.43.222/excuse/api/';
 var apiKey = '123';
+var flag = 0;
 //localhost/excuse/api/getCategories?apiKey=123
 
 	$(document).on(
 			'pageinit',
 			function() {
 				var data = {'apiKey':apiKey};
+				if(0 == flag){
+					flag = 1;
+				} else {
+					return false;
+				}
 				//Get Excuse categories
 				$.ajax({
 					url : url + 'getCategories',
 					type : 'GET',
 					data:data,
-					dataType : "json",
+					dataType : "jsonp",
 					success : function(msg) {
 						console.log(msg);
 						if(msg.success == true) {
@@ -28,19 +35,52 @@ var apiKey = '123';
 							$('#itemsList').listview('refresh');
 							
 						} else {
-							alert(msg.error.message);
+							$('#error-text').html(msg.error.message);
+							$.mobile.changePage('#error','pop',false,true);
+							//alert(msg.error.message);
 						}
 					},
 					error : function(e, f, g) {
 						console.log('error!');
+						$('#error-text').html("Unable to send request. Check Internet connection.");
+						$.mobile.changePage('#error','pop',false,true)
 					}
 				});
 			});
 				
 			$(document).on('vclick', 'li',function(e) {
 				var id = $(this).attr('id');
-				var post = items[id];
-				//console.log(post);
-				$("#blog-title").html(post.title);
-				$("#blog-content").html(post.content);
+				//console.log(id);
+				//alert(id);
+				var data = {'apiKey':apiKey,'categoryId':id,};
+				$.ajax({
+					url : url + 'getExcuses',
+					type : 'GET',
+					data:data,
+					dataType : "json",
+					success : function(msg) {
+						//console.log(msg);
+						if(msg.success == true) {
+							items = msg.data;
+							$.mobile.changePage($("#excuses"),{'allowSamePageTransition':true,'transition':'slide'});
+							for (i = 0; i < items.length; i++) {
+								console.log(items[i].excuse);
+								$("#excuseList").append('<li id="' + items[i].id + '">' 
+									+ items[i].excuse + '</li>');
+							}
+							
+							$('#excuseList').listview('refresh');
+							
+							
+						} else {
+							$('#error-text').html(msg.error.message);
+							$.mobile.changePage('#error','pop',false,true);
+						}
+					},
+					error : function(e, f, g) {
+						console.log('error!');
+						$('#error-text').html("Unable to send request. Check Internet connection.");
+						$.mobile.changePage('#error','pop',false,true)
+					}
+				});
 		    });
